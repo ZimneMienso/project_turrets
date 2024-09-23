77edc7607d58f68a4f969a79892c3f30a30f2c69
Zimny <szpa.general@gmail.com>
2024-09-23 18:00:41 +0200

    Cleaning, documentation and abstraction of turrets
    -base_turret can now acquire targets, aim towards them and manage ammo/reloading logic
    -base_turret.gd has been cleaned and documented
    -Moved tile libraries to their own folder

--- Turrets/modules/hitscan.gd
+++ Turrets/modules/hitscan.gd
@@ -3,14 +3,18 @@ class_name Module_Hitscan
 
 static func project_raycast(
 from:Vector3, to:Vector3, exclude:Array[RID],
-space_state:PhysicsDirectSpaceState3D, collision_mask:int=0b111,
-target_group:String = "units"
+space_state:PhysicsDirectSpaceState3D, collision_mask:int=0b111
 ):
 	var querry = PhysicsRayQueryParameters3D.create(from,to,collision_mask,exclude)
 	var hitscan_intersect:Dictionary = space_state.intersect_ray(querry)
 	if !hitscan_intersect.has("collider"):
 		return
 	var hit_object:Node3D = hitscan_intersect["collider"]
-	if !hit_object.is_in_group(target_group):
-		return
 	return hit_object
+
+static func check_obstacles(
+from:Vector3,to:Vector3,exclue:Array[RID],
+space_state:PhysicsDirectSpaceState3D
+)->bool:
+	var raycast = project_raycast(from,to,exclue,space_state,5)
+	return raycast != null
--- Turrets/turret_scenes/test_turret.gd
+++ Turrets/turret_scenes/test_turret.gd
@@ -1,46 +1,26 @@
 extends Base_Turret
 
-var distance_to_target
-
-var rampup:float:
-	set(new_value):
-		rampup = clamp(new_value,0,1.1)
+@onready var emiter:GPUParticles3D = $x_pivot/muzzle/GPUParticles3D
 
 func _physics_process(delta):
-	if !target or !targets.has(target):
-		target = null
-		rampup -= 1/rampup_time/Engine.physics_ticks_per_second
-		if targets:
-			target = Module_Target_Selection.random(targets)
-	else:
-		var relative = target.global_position - x_pivot.global_position
-
-		var relative_horizontal:Vector2 = Vector2(relative.x,relative.z)
-		var to_target_horizontal = relative_horizontal.angle_to(Vector2.UP)
-		print(to_target_horizontal)
-
-		var relative_vertical:Vector2 = Vector2(relative_horizontal.length(),relative.y)
-		#var to_target_vertical = relative_vertical.angle_to(Vector2.RIGHT)
-		var to_target_vertical = Vector2.RIGHT.angle_to(relative_vertical)
-		
-		rampup += 1/rampup_time/Engine.physics_ticks_per_second
-		y_pivot.rotation.y = clamped_rampup(y_pivot.rotation.y, to_target_horizontal,horizontal_rotation_speed,rampup)
-		x_pivot.rotation.x = clamped_rampup(x_pivot.rotation.x, to_target_vertical,horizontal_rotation_speed,rampup)
-
-		if salvo_ready and abs(y_pivot.rotation.y - to_target_horizontal) < required_accuracy and abs(x_pivot.rotation.x - to_target_vertical) < required_accuracy:
-			var hit_unit:Node3D = Module_Hitscan.project_raycast(x_pivot.global_position,get_barrel_vector(turret_range),[self],get_world_3d().direct_space_state)
-			var debug_line:ImmediateMesh = $debug_line.mesh
-			if hit_unit:
-				if hit_unit.has_method("receive_damage"):
-					hit_unit.receive_damage(damage)
-					distance_to_target = x_pivot.global_position.distance_to(target.global_position)
-					shoot()
-				else:
-					printerr("Unit has no receive_damage() method (test_turret.gd)")
-
+	super(delta)
+	if within_req_acc and salvo_ready:
+		shoot()
 func shoot():
-	super()
-	var emiter:GPUParticles3D = $x_pivot/muzzle/GPUParticles3D
-	var particle_speed = 300
-	emiter.lifetime = distance_to_target/particle_speed
-	emiter.emit_particle(muzzle.global_transform,-muzzle.global_transform.basis.z * particle_speed,Color.WHITE,Color.WHITE,4)
+	var hit_unit:Node3D = Module_Hitscan.project_raycast(
+		x_pivot.global_position, get_barrel_vector(turret_range),
+		[self], get_world_3d().direct_space_state
+	)
+	if hit_unit:
+		if hit_unit.has_method("receive_damage"):
+			super()
+			hit_unit.receive_damage(damage)
+			var distance_to_target = x_pivot.global_position.distance_to(target.global_position)
+			var particle_speed = 600
+			emiter.lifetime = distance_to_target/particle_speed
+			emiter.emit_particle(
+				muzzle.global_transform, Vector3.FORWARD * particle_speed,
+				Color.WHITE, Color.WHITE, 4
+			)
+		else:
+			printerr("Unit has no receive_damage() method (test_turret.gd)")
